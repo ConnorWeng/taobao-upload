1b5f45daadad49187868e343c0b31367035addd8
Connor Weng <foamtea30@gmail.com>
2014-04-21 01:00:11 +0800

    Modify editItem page.

--- Lib/Action/taobao/UploadAction.class.php
+++ Lib/Action/taobao/UploadAction.class.php
@@ -12,9 +12,36 @@ class UploadAction extends CommonAction {
         $taobaoItemId = I('taobaoItemId');
         Util::changeTaoAppkey($taobaoItemId);
         $taobaoItem = $this->checkApiResponse(OpenAPI::getTaobaoItem($taobaoItemId));
-        dump($taobaoItem);
-        $taobaoItemProps = $this->checkApiResponse(OpenAPI::getTaobaoItemProps($taobaoItem->cid));
-        dump($taobaoItemProps);
+        $propsHtml = $this->makePropsHtml($taobaoItem->cid);
+        $this->assign(array(
+            'taobaoItemTitle' => $taobaoItem->title,
+            'propsHtml' => $propsHtml,
+        ));
         $this->display();
+    }
+
+    private function makePropsHtml($cid) {
+        $props = $this->checkApiResponse(OpenAPI::getTaobaoItemProps($cid));
+        $count = count($props->item_prop);
+        $html = '';
+        for ($i = 0; $i < $count; $i++) {
+            $prop = $props->item_prop[$i];
+            $html .= '<li class="J_spu-property" id="spu_'.$prop->pid.'">';
+            $html .= '<label class="label-title">'.$prop->name.':</label>';
+            $html .= '<span><ul class="J_ul-single ul-select"><li>';
+            $html .= '<select name="cp_'.$prop->pid.'" id="prop_'.$prop->pid.'">';
+            $html .= '<option value=""></option>';
+            if (isset($prop->prop_values)) {
+                $valueCount = count($prop->prop_values->prop_value);
+                for ($j = 0; $j < $valueCount; $j++) {
+                    $value = $prop->prop_values->prop_value[$j];
+                    $html .= '<option value="'.$prop->pid.':'.$value->vid.'">'.$value->name.'</option>';
+                }
+            }
+            $html .= '</select>';
+            $html .= '</li></ul></span>';
+            $html .= '</li>';
+        }
+        return $html;
     }
 }
