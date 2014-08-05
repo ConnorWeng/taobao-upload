5b146807e06a16ae93d12657c8f421bbcfc32665
Connor Weng <foamtea30@gmail.com>
2014-08-06 00:51:10 +0800

    Modify store module.

--- Lib/Action/Store/IndexAction.class.php
+++ Lib/Action/Store/IndexAction.class.php
@@ -5,10 +5,29 @@ class IndexAction extends Action {
         $this->display();
     }
 
-    public function findNew() {
+    public function findDiff() {
         $storeVvic = new Model();
-        $rs = $storeVvic->query('select * from ecm_store_vvic where im_ww not in (select im_ww from ecm_store)');
+        $rs = $storeVvic->query("select store_id,shop_mall,floor,address,store_name,see_price,im_qq,im_ww,tel,shop_http from ecm_store_vvic v where not exists (select * from ecm_store s where s.store_name = v.store_name and s.address = v.address and s.im_ww = v.im_ww and s.see_price = v.see_price and s.shop_mall = v.shop_mall) and v.address != ''");
         $this->ajaxReturn($rs);
+    }
+
+    public function findUpdate() {
+        $storeVvic = new Model();
+        $rs = $storeVvic->query("select a.store_id,a.shop_mall,a.floor,a.address,a.store_name,a.see_price,a.im_qq,a.im_ww,a.tel,a.shop_http from (select store_id,shop_mall,floor,address,store_name,see_price,im_qq,im_ww,tel,shop_http from ecm_store_vvic v where not exists (select * from ecm_store s where s.store_name = v.store_name and s.address = v.address and s.im_ww = v.im_ww and s.see_price = v.see_price and s.shop_mall = v.shop_mall) and v.address != '') a inner join (select store_id,shop_mall,floor,address,store_name,see_price,im_qq,im_ww,tel,shop_http from ecm_store_vvic v where exists (select * from ecm_store s where s.address = v.address and s.im_ww = v.im_ww)) b on a.store_id = b.store_id");
+        $this->ajaxReturn($rs);
+    }
+
+    public function findUnused() {
+        $storeVvic = new Model();
+        $rs = $storeVvic->query("select * from ecm_store s where not exists (select * from ecm_store_vvic v where s.im_ww = v.im_ww and s.address = v.address)");
+        $this->ajaxReturn($rs);
+    }
+
+    public function queryStore() {
+        $storeId = $_REQUEST['store_id'];
+        $storeVvic = new Model();
+        $rs = $storeVvic->query("select s.* from ecm_store s inner join ecm_store_vvic v on s.im_ww = v.im_ww and s.address = v.address and v.store_id = {$storeId}");
+        $this->ajaxReturn($rs[0]);
     }
 
     public function addStore() {
@@ -18,23 +37,21 @@ class IndexAction extends Action {
         $this->ajaxReturn($rs);
     }
 
-    public function findDiff() {
-        $storeVvic = new Model();
-        $rs = $storeVvic->query("select v.* from ecm_store_vvic v, ecm_store s where v.im_ww = s.im_ww and (v.see_price != s.see_price or v.shop_mall != s.shop_mall or v.address != s.address)");
-        $this->ajaxReturn($rs);
-    }
-
     public function updateStore() {
         $storeId = $_REQUEST['store_id'];
         $storeVvic = new Model();
-        $rs = $storeVvic->execute("update ecm_store s inner join ecm_store_vvic v on s.im_ww = v.im_ww and v.store_id = {$storeId} set s.see_price = v.see_price, s.shop_mall = v.shop_mall, s.address = v.address");
-        $this->ajaxReturn($rs);
-    }
-
-    public function findUnused() {
-        $storeVvic = new Model();
-        $rs = $storeVvic->query("select * from ecm_store where im_ww not in (select im_ww from ecm_store_vvic)");
-        $this->ajaxReturn($rs);
+        $rs = $storeVvic->query("select * from ecm_store_vvic where store_id = {$storeId}");
+        $seePrice = $rs[0]['see_price'];
+        $shopMall = $rs[0]['shop_mall'];
+        $address = $rs[0]['address'];
+        $imWw = $rs[0]['im_ww'];
+        $storeName = $rs[0]['store_name'];
+        $tel = $rs[0]['tel'];
+        $imQq = $rs[0]['im_qq'];
+        $shopHttp = $rs[0]['shop_http'];
+        $floor = $rs[0]['floor'];
+        $rs2 = $storeVvic->execute("update ecm_store s set s.see_price = '{$seePrice}', s.shop_mall = '{$shopMall}', s.store_name = '{$storeName}', s.tel = '{$tel}', s.im_qq = '{$imQq}', s.shop_http = '{$shopHttp}', s.floor = '{$floor}' where s.im_ww = '{$imWw}' and s.address = '{$address}'");
+        $this->ajaxReturn($rs2);
     }
 
     public function deleteStore() {
