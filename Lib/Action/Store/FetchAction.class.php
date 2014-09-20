321c798a7bd3484ca56a9c4b278ff2981f977ba3
Connor Weng <foamtea30@gmail.com>
2014-09-20 09:07:00 +0800

    Call coffee when fetch store.

--- Lib/Action/Store/FetchAction.class.php
+++ Lib/Action/Store/FetchAction.class.php
@@ -4,6 +4,6 @@ class FetchAction extends Action {
     public function index() {
         header("Content-type:text/html;charset=utf-8");
         $storeId = I('store_id');
-        echo(shell_exec('node /alidata/www/test2/node/51fetch_all/i.js '.$storeId));
+        echo(shell_exec('coffee /alidata/www/test2/node/51fetch_all/single_store.coffee '.$storeId));
     }
 }
