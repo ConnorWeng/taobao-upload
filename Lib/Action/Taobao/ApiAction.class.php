d0e5a0aeb90e3cd82ac2bb86649702609c8e23cf
Connor Weng <foamtea30@gmail.com>
2014-10-10 00:45:34 +0800

    MakePropsHtml api support auto matching props from database.

--- Lib/Action/Taobao/ApiAction.class.php
+++ Lib/Action/Taobao/ApiAction.class.php
@@ -4,9 +4,20 @@ import('@.Util.OpenAPI');
 class ApiAction extends CommonAction {
     public function makePropsHtml() {
         $cid = I('cid');
+        $goodsId = I('goodsId');
         $upload = new UploadAction();
         $props = OpenAPI::getTaobaoItemPropsWithoutVerify($cid);
-        $html = $upload->makePropsHtml($props, '', $cid, false);
+        $propsName = '';
+        if ($goodsId != '') {
+            switch(I('db')) {
+                case 'ecmall':
+                    C('DB_NAME', 'ecmall51');
+                    break;
+            }
+            $taobaoItem = OpenAPI::getTaobaoItemFromDatabase($goodsId);
+            $propsName = $taobaoItem->props_name;
+        }
+        $html = $upload->makePropsHtml($props, $propsName, $cid, false);
         $this->ajaxReturn($html);
     }
 }
