b8edcca03ca291024015ad851b74467943d987a7
Connor Weng <foamtea30@gmail.com>
2015-03-29 15:27:43 +0800

    Add html_decode function.

--- Public/js/common.js
+++ Public/js/common.js
@@ -29,3 +29,16 @@ function yaIndexOf(arr, elt /*, from*/) {
     }
     return -1;
 };
+
+function html_decode(str) {
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&amp;/g, "&");
+    s = s.replace(/&lt;/g, "<");
+    s = s.replace(/&gt;/g, ">");
+    s = s.replace(/&nbsp;/g, " ");
+    s = s.replace(/&#39;/g, "\'");
+    s = s.replace(/&quot;/g, "\"");
+    s = s.replace(/<br>/g, "\n");
+    return s;
+}
