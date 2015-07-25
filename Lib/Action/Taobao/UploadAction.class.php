553b92e71bac0646a6c7933fbdb69a48ff06ba2b
Connor Weng <foamtea30@gmail.com>
2015-07-25 16:14:51 +0800

    Improve parseDescImages function.

--- Lib/Action/Taobao/UploadAction.class.php
+++ Lib/Action/Taobao/UploadAction.class.php
@@ -713,7 +713,7 @@ class UploadAction extends CommonAction {
 
     private function parseDescImages($desc) {
         $cleanDesc = str_replace('<img src="">', '', $desc);
-        $pattern="/(https?:\/\/img\d*\.\w+\.com.+\.(jpg|png|gif|bmp|webp|jpeg))/Ui";
+        $pattern="/(https?:\/\/\w+\.\w+\.com.+\.(jpg|png|gif|bmp|webp|jpeg))/Ui";
         preg_match_all($pattern, $cleanDesc, $matches);
         return $matches[1];
     }
