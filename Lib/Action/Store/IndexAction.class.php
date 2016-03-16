834946021a167efd8511512fe99efc5f98583a3c
Connor Weng <foamtea30@gmail.com>
2016-03-16 22:07:37 +0800

    Improve adjusting stores functionality.

--- Lib/Action/Store/IndexAction.class.php
+++ Lib/Action/Store/IndexAction.class.php
@@ -12,22 +12,21 @@ class IndexAction extends Action {
     }
 
     public function findUpdate() {
-        // $store17zwd = new Model();
-        // $rs = $store17zwd->query("select a.store_id,a.shop_mall,a.floor,a.address,a.store_name,a.see_price,a.im_qq,a.im_ww,a.tel,a.shop_http from (select store_id,shop_mall,floor,address,store_name,see_price,im_qq,im_ww,tel,shop_http from ecm_store_17zwd v where not exists (select * from ecm_store s where s.store_name = v.store_name and s.address = v.address and s.im_ww = v.im_ww and s.see_price = v.see_price and s.shop_mall = v.shop_mall) and v.address != '') a inner join (select store_id,shop_mall,floor,address,store_name,see_price,im_qq,im_ww,tel,shop_http from ecm_store_17zwd v where exists (select * from ecm_store s where s.address = v.address and s.im_ww = v.im_ww)) b on a.store_id = b.store_id");
-        $rs = array();
+        $store17zwd = new Model();
+        $rs = $store17zwd->query("select distinct a.store_id,a.shop_mall,a.floor,a.address,a.store_name,a.see_price,a.im_qq,a.im_ww,a.tel,a.shop_http from ecm_store_17zwd a, ecm_store b where a.im_ww = b.im_ww and (a.shop_mall != b.shop_mall or a.floor != b.floor or a.address != b.address) limit 1000");
         $this->ajaxReturn($rs);
     }
 
     public function findUnused() {
         $store17zwd = new Model();
-        $rs = $store17zwd->query("select store_id, shop_mall, floor, address, store_name, see_price, im_qq, im_ww, tel, shop_http from ecm_store s where state = 1 and not exists (select 1 from ecm_store_17zwd v where s.im_ww = v.im_ww)");
+        $rs = $store17zwd->query("select store_id, shop_mall, floor, address, store_name, see_price, im_qq, im_ww, tel, shop_http from ecm_store s where not exists (select 1 from ecm_store_17zwd v where s.im_ww = v.im_ww)");
         $this->ajaxReturn($rs);
     }
 
     public function queryStore() {
         $storeId = $_REQUEST['store_id'];
         $store17zwd = new Model();
-        $rs = $store17zwd->query("select s.* from ecm_store s inner join ecm_store_17zwd v on s.im_ww = v.im_ww and s.address = v.address and v.store_id = {$storeId}");
+        $rs = $store17zwd->query("select s.* from ecm_store s inner join ecm_store_17zwd v on s.im_ww = v.im_ww and v.store_id = {$storeId}");
         $this->ajaxReturn($rs[0]);
     }
 
@@ -62,14 +61,14 @@ class IndexAction extends Action {
         $imQq = $rs[0]['im_qq'];
         $shopHttp = $rs[0]['shop_http'];
         $floor = $rs[0]['floor'];
-        $rs2 = $store17zwd->execute("update ecm_store s set s.see_price = '{$seePrice}', s.shop_mall = '{$shopMall}', s.store_name = '{$storeName}', s.tel = '{$tel}', s.im_qq = '{$imQq}', s.shop_http = '{$shopHttp}', s.floor = '{$floor}', s.state = 1 where s.im_ww = '{$imWw}' and s.address = '{$address}'");
+        $rs2 = $store17zwd->execute("update ecm_store s set s.see_price = '{$seePrice}', s.shop_mall = '{$shopMall}', s.store_name = '{$storeName}', s.tel = '{$tel}', s.im_qq = '{$imQq}', s.shop_http = '{$shopHttp}', s.floor = '{$floor}', s.address = '{$address}', s.dangkou_address = '{$address}', s.state = 1 where s.im_ww = '{$imWw}'");
         $this->ajaxReturn($rs2);
     }
 
     public function deleteStore() {
         $storeId = $_REQUEST['store_id'];
         $store17zwd = new Model();
-        $rs = $store17zwd->execute("update ecm_store set state = 9 where store_id = {$storeId}");
+        $rs = $store17zwd->execute("update ecm_store set state = 2, close_reason = 'according to 17zwd' where store_id = {$storeId}");
         $this->ajaxReturn($rs);
     }
 }
